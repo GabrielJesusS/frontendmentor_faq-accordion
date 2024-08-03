b138d27c99ef9a851a5d3506ffe8c714ea5eeb11
Gabriel Jesus <gabrieljesus9221@gmail.com>
2024-08-03 11:31:07 -0300

    feat: adds accordion partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./source/**/*.{html,js,ts}"],
   theme: {
     extend: {
+      maxWidth: {
+        "accordion-container": "37.5rem",
+      },
       fontFamily: {
         "work-sans": ["work-sans", "serif"],
       },
@@ -10,6 +13,11 @@ module.exports = {
         "light-pink": "rgba(249 240 255 / <alpha-value>)",
         "grayish-purple": "rgba(140 105 145 / <alpha-value>)",
         "dark-purple": "rgba(47 21 51 / <alpha-value>)",
+      },
+      backgroundImage: {
+        "mobile-pattern": "url(../public/images/background-pattern-mobile.svg)",
+        "desktop-pattern":
+          "url(../public/images/background-pattern-desktop.svg)",
       },
     },
   },
